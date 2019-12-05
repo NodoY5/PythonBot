e07abeb80706df94077bf6dc41a0757af63546a5
Devashrit Sarangi <woozydragon@outlook.com>
2019-12-05 19:00:23 +0530

    Commit First

--- bot.py
+++ bot.py
@@ -1,8 +1,14 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 import os
 import random
+import json
 from discord.utils import get
+from itertools import cycle
+
+extension_file = "extensions.json"
+with open(extension_file) as file:
+    extensions = json.load(file)["extensions"]
 
 
 def get_prefix(bot, msg):
@@ -13,12 +19,18 @@ def get_prefix(bot, msg):
 bot=commands.Bot(case_insensitive=True,command_prefix=get_prefix)
 bot.remove_command('help')
 
-@bot.event
+status = cycle(['PyBot v1.0!', 'with WoozyDragon', 'VLC Media Player', 'Ludo', 'Snakes and Ladders', 'Space Shuttle', 'ISRO', 'Human Legacy by Ivan Torrent'])
+
+@bot.listen()
 async def on_ready():
+    change_status.start()
     print("Bot is ready for action")
 
+@tasks.loop(seconds=12)
+async def change_status():
+    await bot.change_presence(activity=discord.Game(next(status)))
 
-@bot.event
+@bot.listen()
 async def on_member_join(member):
     role = discord.utils.get(member.guild.roles, name='Member')
     await member.add_roles(role)
@@ -28,7 +40,7 @@ async def on_member_join(member):
     embed.add_field(name=f'New member!', value=f'{member} has joined this server!', inline=False)
     await channel.send(embed=embed)
 
-@bot.command(pass_context = True)
+@bot.command()
 async def help(ctx):
     embed=discord.Embed(color=0xFFFF)
     embed.set_author(name='PyBot Help')
@@ -36,11 +48,12 @@ async def help(ctx):
     embed.add_field(name='?calchelp', value='Help for Calculator Commands', inline=False)
     embed.add_field(name='?pms', value='PyBot Messaging Service (PMS) [?pms @<user.mention> <your_message_here>]', inline=False)
     embed.add_field(name='?coinflip', value='Flips a coin for you', inline=False)
-    embed.add_field(name='?diceroll', value='Rolls a dice for you', inline=False)
-    embed.set_footer(text='PyBot v0.5')
+    embed.add_field(name='?diceroll', value='Rolls a dice', inline=False)
+    embed.add_field(name='?suggest', value='Suggest for the server [?suggest <suggestion>]', inline=False)
+    embed.set_footer(text='PyBot v1')
     await ctx.send(embed=embed)
 
-@bot.command(pass_context = True)
+@bot.command()
 async def calchelp(ctx):
     embed=discord.Embed(color=0x00ff00)
     embed.set_author(name='PyBot Calculator Help')
@@ -51,7 +64,7 @@ async def calchelp(ctx):
     await ctx.send(embed=embed)
 
 @commands.has_role("Staff")
-@bot.command(pass_context = True)
+@bot.command()
 async def modhelp(ctx):
     embed=discord.Embed(color=0x0000ff)
     embed.set_author(name='PyBot Moderation Help')
@@ -87,7 +100,7 @@ async def d(ctx, numi, numii):
     await ctx.send(str(numi) + ' / ' + str(numii) + ' = ' + str(sum_value))
 
 @commands.has_role("Staff")
-@bot.command(pass_context = True)
+@bot.command()
 async def clear(ctx,amount:int=0):
     if ctx.author.guild_permissions.manage_messages:
         await ctx.channel.purge(limit=amount+1)
@@ -98,7 +111,7 @@ async def clear(ctx,amount:int=0):
         embed.add_field(name='Clear Command used', value=f'{ctx.author.mention} has used `purge` command.')
         await channel.send(embed=embed)
 
-@bot.command(pass_context=True)
+@bot.command()
 async def coinflip(ctx):
     flip = [
         'You got **heads**',
@@ -107,17 +120,16 @@ async def coinflip(ctx):
 
     await ctx.send(random.choice(flip))
 
-@bot.command(pass_context=True)
+@bot.command()
 async def diceroll(ctx):
     roll = [
-    '1', '2', '3', '4', '5', '6'
+        '1','2','3','4','5','6'
     ]
 
     await ctx.send(random.choice(roll))
 
-
 @commands.has_role("Staff")
-@bot.command(pass_context=True)
+@bot.command()
 async def warn(ctx, user: discord.User, *, reason=None):
     await ctx.channel.purge(limit=1)
     embeda = discord.Embed(color=0xFFFF)
@@ -142,9 +154,17 @@ async def pms(ctx, user: discord.User, *, message=None):
     await user.send(embed=embed)
     await ctx.send(f'{ctx.author.mention}, Succesfully sent your message to ' + str(user) + ' which says : ' + str(message))
 
+@bot.command(pass_context = True)
+async def suggest(ctx, *, suggest=None):
+    channel = bot.get_channel(648929055151882241)
+    embed = discord.Embed(color=0xffff00)
+    embed.set_author(name=f'{ctx.author}')
+    embed.add_field(name='Suggestion:', value=suggest, inline=False)
+    embed.set_footer(text='PyBot Suggestions')
+    await channel.send(embed=embed)
 
 @commands.has_role("Staff")
-@bot.command(pass_context=True)
+@bot.command()
 async def kick(ctx, user:discord.Member, *, reason=None):
     embeda = discord.Embed(color=0xFFFF)
     embeda.set_author(name='Kicked!')
@@ -160,11 +180,11 @@ async def kick(ctx, user:discord.Member, *, reason=None):
     await channel.send(embed=embed)
 
 @commands.has_role("Staff")
-@bot.command(pass_context=True)
+@bot.command()
 async def ban(ctx, user:discord.Member, *, reason=None):
     embeda = discord.Embed(color=0xFFFF)
-    embeda.set_author(name='Kicked!')
-    embeda.add_field(name='You were kicked from Elite Programmers Group for :', value=reason, inline=False)
+    embeda.set_author(name='Banned!')
+    embeda.add_field(name='You were Banned from Elite Programmers Group for :', value=reason, inline=False)
     await user.send(embed=embeda)
     await user.ban(reason=reason)
     await ctx.send(str(user) + ' has succesfully been banned for : ' + reason)
@@ -175,7 +195,7 @@ async def ban(ctx, user:discord.Member, *, reason=None):
     embed.add_field(name='Reason : ', value=reason, inline=False)
     await channel.send(embed=embed)
 
-@bot.event
+@bot.listen()
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
         embed = discord.Embed(color=0xff0000)
@@ -189,11 +209,14 @@ async def on_command_error(ctx, error):
         embed.add_field(name='Permissions', value='You don\'t have the permissions to run this command!', inline=False)
         await ctx.send(embed=embed)
 
-    if isinstance(error, commands.MissingRequiredArguments):
-        embed = discord.Embed(color=0xff0000)
-        embed.set_author(name='Error!')
-        embed.add_field(name='Arguments required!', value='Please pass in all required arguments!', inline=False)
-        await ctx.send(embed=embed)
 
+
+if len(extensions) > 0:
+    for ext in extensions:
+        try:
+            bot.load_extension(ext)
+            print(f"Extension {ext} loaded successfully")
+        except Exception:
+            print(f"Extension {ext} failed to load")
 
 bot.run(os.getenv('token'))
